e9ad56695747e619fde779816cd457ad7e47ed66
Flavio Silva <flavioandre.2001.9.7@gmail.com>
2021-03-09 22:06:00 +0000

    Show UI Status

--- src/Script/index.js
+++ src/Script/index.js
@@ -1,29 +1,15 @@
 // Setup
+// Status UI
+const healthStatus = document.getElementById("HealthStatus");
+const hungryStatus = document.getElementById("HungryStatus");
+const energyStatus = document.getElementById("EnergyStatus");
+const hygieneStatus = document.getElementById("HygieneStatus");
+const board = document.getElementById("board");
+
 let HealthPoints = 100;
 let HungryPoints = 100;
 let EnergyPoints = 100;
 let HygienePoints = 100;
-
-// Remove status
-let healthPointsUpdate = setInterval(() => {
-	if (HealthPoints) HealthPoints--;
-	update();
-}, 10000);
-
-let hungryPointsUpdate = setInterval(() => {
-	if (HungryPoints) HungryPoints--;
-	update();
-}, 1000);
-
-let energyPointsUpdate = setInterval(() => {
-	if (EnergyPoints) EnergyPoints--;
-	update();
-}, 1000);
-
-let hygienePointsUpdate = setInterval(() => {
-	if (HygienePoints) HygienePoints--;
-	update();
-}, 1000);
 
 // Add (Buttons Click)
 const btnGym = document.getElementById("btnGym");
@@ -32,43 +18,105 @@ const btnBedroom = document.getElementById("btnBedroom");
 const btnBathroom = document.getElementById("btnBathroom");
 
 btnGym.addEventListener("click", () => {
-	EnergyPoints -= 25;
-	HungryPoints -= 15;
-	HygienePoints -= 20;
-	HealthPoints += 20;
-	update();
+	updateHealth(20);
+	updateHungry(-15);
+	updateEnergy(-25);
+	updateHygiene(-20);
 });
 
 btnKitchen.addEventListener("click", () => {
-	HygienePoints -= 10;
-	EnergyPoints -= 5;
-	HungryPoints += 25;
-	HealthPoints += 5;
-	update();
+	updateHealth(5);
+	updateHungry(25);
+	updateEnergy(-5);
+	updateHygiene(-10);
 });
 
 btnBedroom.addEventListener("click", () => {
-	HungryPoints -= 15;
-	EnergyPoints += 50;
-	HealthPoints += 5;
-	update();
+	updateHealth(5);
+	updateHungry(-15);
+	updateEnergy(50);
 });
 
 btnBathroom.addEventListener("click", () => {
-	EnergyPoints -= 1;
-	update();
+	updateEnergy(-1);
+	updateHygiene(50);
 });
 
-// Update Status
-const healthStatus = document.getElementById("HealthStatus");
-const hungryStatus = document.getElementById("HungryStatus");
-const energyStatus = document.getElementById("EnergyStatus");
-const hygieneStatus = document.getElementById("HygieneStatus");
+// Update Status // Add / Remove Status Functions
+updateHealth();
+function updateHealth(value = 0) {
+	HealthPoints += value;
+	if (HealthPoints <= 0) {
+		HealthPoints = 0;
+		// alert("O teu " + monsterName.textContent + " foi desta para melhor :(");
+		console.log("O teu " + monsterName.textContent + " foi desta para melhor :(");
+	} else if (HealthPoints >= 100) HealthPoints = 100;
 
-update();
-function update() {
 	healthStatus.title = HealthPoints;
-	hungryStatus.title = HungryPoints;
-	energyStatus.title = EnergyPoints;
-	hygieneStatus.title = HygienePoints;
+
+	changeColorUI(healthStatus, HealthPoints);
+	changeColorUI(board, HealthPoints, true);
 }
+
+updateHungry();
+function updateHungry(value = 0) {
+	HungryPoints += value;
+	if (HungryPoints <= 0) {
+		HungryPoints = 0;
+		updateHealth(-1);
+	} else if (HungryPoints >= 100) HungryPoints = 100;
+
+	hungryStatus.title = HungryPoints;
+
+	changeColorUI(hungryStatus, HungryPoints);
+}
+
+updateEnergy();
+function updateEnergy(value = 0) {
+	EnergyPoints += value;
+	if (EnergyPoints <= 0) {
+		EnergyPoints = 0;
+		updateHealth(-1);
+	} else if (EnergyPoints >= 100) EnergyPoints = 100;
+
+	energyStatus.title = EnergyPoints;
+
+	changeColorUI(energyStatus, EnergyPoints);
+}
+
+updateHygiene();
+function updateHygiene(value = 0) {
+	HygienePoints += value;
+	if (HygienePoints <= 0) {
+		HygienePoints = 0;
+		updateHealth(-1);
+	} else if (HygienePoints >= 100) HygienePoints = 100;
+
+	hygieneStatus.title = HygienePoints;
+
+	changeColorUI(hygieneStatus, HygienePoints);
+}
+
+function changeColorUI(element, value, isBoard = false) {
+	let newColor = "green";
+	if (value >= 90) newColor = "green";
+	else if (value < 90 && value >= 60) newColor = "yellow";
+	else if (value < 60 && value >= 40) newColor = "orange";
+	else if (value < 40 && value >= 1) newColor = "red";
+	else if (value == 0) newColor = "black";
+
+	if (isBoard) element.style.borderColor = newColor;
+	else element.style.backgroundColor = newColor;
+}
+
+// Remove status
+let healthPointsUpdate = setInterval(() => updateHealth(-1), 10000);
+let hungryPointsUpdate = setInterval(() => updateHungry(-1), 1000);
+let energyPointsUpdate = setInterval(() => updateEnergy(-1), 1000);
+let hygienePointsUpdate = setInterval(() => updateHygiene(-1), 1000);
+
+// Change Player/Monster Name
+const monsterName = document.getElementById("MonsterName");
+monsterName.addEventListener("click", () => {
+	monsterName.textContent = prompt("Novo nome: ", monsterName.textContent) || monsterName.textContent;
+});
